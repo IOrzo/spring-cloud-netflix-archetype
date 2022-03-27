5d2b45f919ad3e22b88d987df3d0c333a1e513ed
xx <369889186@qq.com>
2022-03-27 10:55:27 +0800

    通过官方打包好的 jar 包本地引入 apollo 配置中心

---
+++ archetype-apollo/custom-apollo-adminsrvice.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+printf "idea中启动，自制脚本，方便查看日志"
+cd apollo-adminservice-1.9.2-github
+java -Xms200m -Xmx200m -Duser.timezone=Asia/Shanghai -Dclient.encoding.override=UTF-8 -Dfile.encoding=UTF-8 -jar apollo-adminservice-1.9.2.jar
+exit 0;
