f474503f708ed5d8a7ffe4cbdf3158b1f53c2567
xx <369889186@qq.com>
2022-03-27 10:42:20 +0800

    #本地搭建 eureka 和 zuul

---
+++ archetype-zuul/src/test/java/com/sixtofly/archetype/zuul/ArchetypeZuulApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.sixtofly.archetype.zuul;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ArchetypeZuulApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
